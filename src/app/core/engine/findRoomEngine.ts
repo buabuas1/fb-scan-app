94afbfe411f9cb26cc1d9367860b3b1331260588
sonnv <hailos1000@gmail.com>
2020-09-27 20:10:33 +0700

    update logic

--- src/app/core/engine/findRoomEngine.ts
+++ src/app/core/engine/findRoomEngine.ts
@@ -3,7 +3,7 @@ import {IBDSModel} from '../../common/model/facebook/IBDS.model';
 
 export class FindRoomEngine implements IBDSType {
     private regex = /Cần tìm phòng|tìm phòng|muốn thuê phòng|tìm phòg|tìm nhà|cần thuê|Cần tìm|tìm trọ/muig;
-    private negativeRegex = /24\/7|24\/24/muig;
+    private negativeRegex = /24\/7|24\/24|bên mình còn/muig;
     private commentFindRoom = /xin giá|inb|inbbox|ib/muig;
     validateType(entity: IBDSModel): boolean {
         return this.regex.test(entity.content) && !this.negativeRegex.test(entity.content)
